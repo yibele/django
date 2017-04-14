62b42689e069d1b0b1a5e0e472c41194002273c7
yibeel <yibeel@163.com>
2017-04-14 09:43:52 +0800

    lesson 20

--- music/models.py
+++ music/models.py
@@ -6,10 +6,16 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.album_title + '-' + self.artist
+
 class Song(models.Model):
     #ling the song to the album
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title
 
 
--- music/views.py
+++ music/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Album
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage </h1>")
+    all_albums = Album.objects.all()
+
+    context = {
+            'all_albums':all_albums,
+        }
+
+    return render(request, 'music/index.html',context)
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html',{'album':album})
+
+
 
